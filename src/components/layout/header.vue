93dbfacecf46ab1731e396c51ca06f94660643c9
Sergio <Checcarelli>
2020-11-13 19:47:20 -0300

    Primer commit del proyecto completo

---
+++ src/components/layout/header.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-start
+            .nav-item
+              strong 🎼Platzi Music
+          .nav-end.nav-menu
+            router-link.nav-item(to="/") Buscar
+            router-link.nav-item(to="about") Nosotros
+    .hero.body
+      .container.has-text-centered
+        h1.title Platzi Music
+        h2.subtitle Canciones que estan Vue-nisimas!!!
+        pm-player
+</template>
+
+<script>
+import PmPlayer from '@/components/Player.vue'
+
+export default {
+  components: { PmPlayer }
+}
+</script>
